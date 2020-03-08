ed947acc42346459d1ece4eb8c2573fe0b3806b6
Dmitry Moiseenko <kiosolion@gmail.com>
2020-03-08 00:12:56 +0000

    Basic key handle and output

--- client/app.js
+++ client/app.js
@@ -1,15 +1,67 @@
 let ws = new WebSocket("ws://localhost:8080");
+let inputText = "";
+let terminal = document.getElementById('terminal');
 
 ws.onmessage = function(data) {
-    console.log(data);
-    var incomingMessage = data.data;
+    //console.log(data);
+    let incomingMessage = data.data;
     showMessage(incomingMessage);
 }
 
 function showMessage(message) {
-    var messageElem = document.createElement('div');
-    messageElem.appendChild(document.createTextNode(message));
-    document.getElementById('terminal').appendChild(messageElem);
+    let messagesplit = message.split('\n');
+    let first = messagesplit.shift();
+
+    if(!terminal.lastElementChild){
+        let messageElem = document.createElement('div');
+        messageElem.appendChild(document.createTextNode(first));
+        terminal.appendChild(messageElem);
+    } else {
+        terminal.lastElementChild.innerText += first;
+        //terminal.lastElementChild.style.background = "yellow";
+    }
+
+    for(let it of messagesplit){
+        let messageElem = document.createElement('div');
+        messageElem.appendChild(document.createTextNode(it));
+        terminal.appendChild(messageElem);
+    }
 }
 
-ws.onopen = () => ws.send('echo "hello world!"\n');
+
+
+//kinput.onkeydown = kinput.onkeyup = kinput.onkeypress = handle;
+let lastTime = Date.now();
+
+function handle(e) {
+    /*
+    let text = e.type +
+            ' key=' + e.key +
+            ' code=' + e.code +
+            (e.shiftKey ? ' shiftKey' : '') +
+            (e.ctrlKey ? ' ctrlKey' : '') +
+            (e.altKey ? ' altKey' : '') +
+            (e.metaKey ? ' metaKey' : '') +
+            (e.repeat ? ' (repeat)' : '') +
+            "\n";
+    */
+    if(e.type == "keydown"){
+        if(e.key.length == 1){
+            ws.send(e.key);
+        } else if(e.key == "Enter") {
+            ws.send('\n');
+        }
+    }
+
+    lastTime = Date.now();
+
+    /*area.value += text;
+
+    if (form.elements[e.type + 'Stop'].checked) {
+            e.preventDefault();
+    }*/
+}
+
+//function keyListener(e){}
+addEventListener("keydown", handle);
+//ws.onopen = () => ws.send('echo "hello world!"\n');
