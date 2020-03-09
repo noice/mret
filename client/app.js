5be884b46ffaac3a2a61e402236206504e90317a
Dmitry Moiseenko <kiosolion@gmail.com>
2020-03-09 00:17:53 +0000

    regexp

--- client/app.js
+++ client/app.js
@@ -1,6 +1,12 @@
-let ws = new WebSocket("ws://localhost:8080");
-let inputText = "";
-let terminal = document.getElementById('terminal');
+var ws = new WebSocket("ws://localhost:8080");
+var inputText = "";
+var terminal = document.getElementById('terminal');
+
+var dcolor = 'white';
+var acolor = dcolor;
+
+var dbgcolor = 'black';
+var abgcolor = dbgcolor;
 
 ws.onmessage = function(data) {
     //console.log(data);
@@ -8,24 +14,70 @@ ws.onmessage = function(data) {
     showMessage(incomingMessage);
 }
 
-function showMessage(message) {
-    let messagesplit = message.split('\n');
-    let first = messagesplit.shift();
+function handleEscape(message) {
+    /*switch(message[f]){
+        case 'm':
+            if (f == i || (message[i] == '0' && i + 1 == f)){
+                acolor = dcolor;
+                abgcolor = dbgcolor;
+            }
+            break;
+        default:
+            break;
+    }*/
+    let result = message.match(/\x1B\[\??(([0-9]*)*(;([0-9]*)*))([ABCDEFGHJKSTfmnsulh])/);
 
-    if(!terminal.lastElementChild){
-        let messageElem = document.createElement('div');
-        messageElem.appendChild(document.createTextNode(first));
-        terminal.appendChild(messageElem);
-    } else {
-        terminal.lastElementChild.innerText += first;
-        //terminal.lastElementChild.style.background = "yellow";
+
+    if(!result)
+        return 0;
+
+    console.log(result[0]);
+
+
+    return result[0].length - 1;
+}
+
+function showMessage(message) {
+    /*let messagesplit = message.split('\n');
+
+    if(terminal.lastElementChild){
+        terminal.lastElementChild.innerText += messagesplit.shift();
     }
 
     for(let it of messagesplit){
         let messageElem = document.createElement('div');
         messageElem.appendChild(document.createTextNode(it));
         terminal.appendChild(messageElem);
+    }*/
+    
+    if(!terminal.lastElementChild){
+        terminal.appendChild(document.createElement('div'));
     }
+
+    for(let i = 0; i < message.length; i ++){
+        switch(message[i]){
+            case '\x1B':
+                if(message[i + 1] != '[')
+                    break;
+                
+                i += handleEscape(message.slice(i));
+
+                break;
+
+            case '\n':
+                terminal.appendChild(document.createElement('div'));
+                break;
+
+            default:
+                let charElem = document.createElement('span');
+                charElem.appendChild(document.createTextNode(message[i]));
+                charElem.style.color = acolor;
+                charElem.style.backgroundColor = dbgcolor;
+                terminal.lastElementChild.appendChild(charElem);
+                break;
+        }
+    }
+
 
     window.scrollTo(0,document.body.scrollHeight);
 }
